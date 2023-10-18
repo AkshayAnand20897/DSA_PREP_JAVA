e01be5f38a553d4b9564af061df4a06b553ab1ee
akshay <akshay.anand@karexpert.com>
2023-10-19 00:37:12 +0530

    added changes

--- src/NumberExample.java
+++ src/NumberExample.java
@@ -1,5 +1,23 @@
 public class NumberExample {
     public static void main(String[] args) {
-        System.out.println("This is my first file on git");
+       // System.out.println("This is my first file on git");
+        print1(1);
+
+
+    }
+    static void print1(int n){
+        System.out.println(n);
+        print2(2);
+    }
+    static void print2(int n){
+        System.out.println(n);
+        print3(3);
+    }
+    static void print3(int n){
+        System.out.println(n);
+        print4(4);
+    }
+    static void print4(int n){
+        System.out.println(n);
     }
 }
