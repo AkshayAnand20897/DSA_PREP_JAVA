6f7417040bb602e7a031474dd2af1bad9e532adb
akshay <akshay.anand@karexpert.com>
2023-10-20 21:37:19 +0530

    added fibbonacci

--- src/Fibbonacci.java
+++ src/Fibbonacci.java
@@ -6,11 +6,14 @@ public class Fibbonacci {
 
 
     static int fib(int n){
-        if(n==0){
-            return 0;
-        }
-        if(n==1){
-            return 1;
+//        if(n==0){
+//            return 0;
+//        }
+//        if(n==1){
+//            return 1;
+//        }
+        if(n<2){
+            return n;
         }
         return fib(n-1)+fib(n-2);
     }
